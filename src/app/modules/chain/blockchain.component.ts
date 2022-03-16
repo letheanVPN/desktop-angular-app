f70bc29f6642aff4b78652f0d4f943a6d38c326d
Snider <snider@lt.hn>
2022-03-16 08:45:22 +0000

    lets you paginate the chain page back to the start converting page numbers to height ranges

--- src/app/modules/chain/blockchain.component.ts
+++ src/app/modules/chain/blockchain.component.ts
@@ -7,7 +7,7 @@ import {interval, Observable, Subscription} from 'rxjs';
 import {BlockHeader} from '@module/chain/interfaces/types/blockHeader';
 import {ColumnMode} from '@swimlane/ngx-datatable';
 import {BlockchainService} from '@module/chain/blockchain.service';
-import {FormControl, Validators} from "@angular/forms";
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
 	selector: 'lthn-chain',
@@ -16,44 +16,51 @@ import {FormControl, Validators} from "@angular/forms";
 })
 export class BlockchainComponent implements OnInit, OnDestroy {
 
-	columns: any = []
+	columns: any = [];
 
+	page = {
+		size: 25,
+		totalElements: 0,
+		totalPages: 0,
+		pageNumber: 0
+	};
 
-	public allColumns = [
-		{ prop: 'height', name: 'plugin.lthn.chain.table.th.height', default: true},
-		{ prop: 'difficulty', name: 'plugin.lthn.chain.table.th.difficulty', default: true },
-		{ prop: 'hash', name: 'plugin.lthn.chain.table.th.hash', default: true  },
-		{ prop: 'orphan_status', name: 'plugin.lthn.chain.table.th.orphan_status', default: true },
+	allColumns = [
+		{prop: 'height', name: 'plugin.lthn.chain.table.th.height', default: true},
+		{prop: 'difficulty', name: 'plugin.lthn.chain.table.th.difficulty', default: true},
+		{prop: 'hash', name: 'plugin.lthn.chain.table.th.hash', default: true},
+		{prop: 'orphan_status', name: 'plugin.lthn.chain.table.th.orphan_status', default: true},
 		//{ prop: 'reward', name: 'plugin.lthn.chain.table.th.reward', default: true },
-		{ prop: 'timestamp', name: 'plugin.lthn.chain.table.th.timestamp', default: true },
-	//	{ prop: 'block_size', name: 'Block Size', default: true },
+		{prop: 'timestamp', name: 'plugin.lthn.chain.table.th.timestamp', default: true}
+		//	{ prop: 'block_size', name: 'Block Size', default: true },
 		//{ prop: 'depth', name: 'plugin.lthn.chain.table.th.depth', default: true },
 		//{ prop: 'major_version', name: 'Block Major Version', default: true },
-	//	{ prop: 'minor_version', name: 'Block Minor Version', default: false },
-	//	{ prop: 'nonce', name: 'Block Nonce', default: false },
-	//	{ prop: 'num_txes', name: 'Transactions', default: true },
-	//	{ prop: 'prev_hash', name: 'Last Hash', default: true },
-	]
+		//	{ prop: 'minor_version', name: 'Block Minor Version', default: false },
+		//	{ prop: 'nonce', name: 'Block Nonce', default: false },
+		//	{ prop: 'num_txes', name: 'Transactions', default: true },
+		//	{ prop: 'prev_hash', name: 'Last Hash', default: true },
+	];
 	ColumnMode = ColumnMode;
-	public blocks: Observable<{ headers: BlockHeader[]}>;
-	public chainInfo: Observable<ChainGetInfo>;
+	blocks: Observable<{ headers: BlockHeader[] }>;
+	chainInfo: Observable<ChainGetInfo>;
 
 	private sub: Subscription[] = [];
-	public recentTxs: any;
-	public buildType: string;
-	public status_daemon: number = 0;
-	 blockSearch: FormControl;
-	constructor( private store: Store,
-				 private chain: BlockchainService) {
+	recentTxs: any;
+	buildType: string;
+	status_daemon: number = 0;
+	blockSearch: FormControl;
+
+	constructor(private store: Store,
+				private chain: BlockchainService) {
 
 		this.allColumns.forEach((col) => {
-			if (col.default){
+			if (col.default) {
 
 				this.columns.push(
 					col
-				)
+				);
 			}
-		})
+		});
 	}
 
 
@@ -61,24 +68,31 @@ export class BlockchainComponent implements OnInit, OnDestroy {
 		this.blockSearch = new FormControl('', [Validators.required]);
 
 		//this.chain.getInfo()
-		this.chainInfo = this.store.pipe(select(getChainInfo))
-		this.blocks = this.store.pipe(select(getChainBlocks))
+		this.chainInfo = this.store.pipe(select(getChainInfo));
+		this.blocks = this.store.pipe(select(getChainBlocks));
 
 		this.sub['interval'] = interval(5000).subscribe(() => this.chain.getInfo());
 		this.sub['info'] = this.store.pipe(select(getChainInfo)).subscribe((data) => {
-			if(data){
+			if (data) {
+				console.log(this.page)
 				// we have chain data, and it talks to us set to amber
-				this.status_daemon = 1
+				this.status_daemon = 1;
 				//console.log(data)
 				// if chain height + 4 to give 2~ blocks to be considered healthy
-				if(data.height + 4 > data.target_height){
-					this.status_daemon = 2
+				if (data.height + 4 > data.target_height) {
+					this.status_daemon = 2;
 				}
-				this.chain.getBlocks(data.height - 50, data.height - 1);
-			}else{
-				this.status_daemon = 0
+				this.page.totalElements = data.height
+				console.log((data.height - this.page.size ) - (this.page.size - (this.page.size*this.page.pageNumber)), data.height - (this.page.size*this.page.pageNumber)-1)
+				this.getBlocks((data.height - this.page.size ) - (this.page.size - (this.page.size*this.page.pageNumber)), data.height - (this.page.size*this.page.pageNumber)-1);
+			} else {
+				this.status_daemon = 0;
 			}
 		});
+	}
+
+	getBlocks(start_height, end_height){
+		this.chain.getBlocks(start_height, end_height);
 	}
 
 	toggle(col) {
