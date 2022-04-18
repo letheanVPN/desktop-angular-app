6e1876146c54e11babea2309884b2105ea1f7d8c
Snider <Snider@users.noreply.github.com>
2022-04-18 15:58:59 +0000

    deploy: 6cdbb98e6878ec8dc4adbc683184005c998fc13d

--- js/menu-wc.js
+++ js/menu-wc.js
@@ -469,9 +469,6 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 <a href="components/DetailsComponent.html" data-type="entity-link" >DetailsComponent</a>
                             </li>
                             <li class="link">
-                                <a href="components/ModalComponent.html" data-type="entity-link" >ModalComponent</a>
-                            </li>
-                            <li class="link">
                                 <a href="components/OpenComponent.html" data-type="entity-link" >OpenComponent</a>
                             </li>
                             <li class="link">
@@ -547,9 +544,6 @@ customElements.define('compodoc-menu', class extends HTMLElement {
                                 </li>
                                 <li class="link">
                                     <a href="injectables/FileSystemWebService.html" data-type="entity-link" >FileSystemWebService</a>
-                                </li>
-                                <li class="link">
-                                    <a href="injectables/ModalComponent.html" data-type="entity-link" >ModalComponent</a>
                                 </li>
                                 <li class="link">
                                     <a href="injectables/NetworkHashrateChart.html" data-type="entity-link" >NetworkHashrateChart</a>
