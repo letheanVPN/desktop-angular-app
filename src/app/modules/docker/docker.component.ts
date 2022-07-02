b5e614ca4a812e4be107474057a2218756aa05fb
Snider <snider@lt.hn>
2022-07-02 17:48:42 +0100

    adds docker container creation for lthn/chain

--- src/app/modules/docker/docker.component.ts
+++ src/app/modules/docker/docker.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {NotificationService, NotificationStyleType, NotificationType} from '@swimlane/ngx-ui';
+import {interval, Subscription} from 'rxjs';
 
 @Component({
   selector: 'lthn-app-docker',
   templateUrl: './docker.component.html',
   styleUrls: ['./docker.component.scss']
 })
-export class DockerComponent implements OnInit {
+export class DockerComponent implements OnInit, OnDestroy {
 
   public containers = [];
+  private sub: Subscription;
   constructor(private notificationService: NotificationService) { }
 
   async ngOnInit() {
+    this.containers = await this.getContainerList()
+    this.sub = interval(50000).subscribe(async () => {
+        this.containers = await this.getContainerList()
 
+      });
+
+  }
+
+  async getContainerList(){
     const containers = await fetch('http://localhost:36911/docker/containers', {
       method: "GET",
       headers: {
@@ -20,7 +30,7 @@ export class DockerComponent implements OnInit {
       }
     })
     this.containers = await containers.json()
-    console.log(this.containers)
+    return this.containers;
   }
 
   async startContainer(containerId) {
@@ -38,6 +48,7 @@ export class DockerComponent implements OnInit {
       title: 'Start Requested!',
       body: containerId
     })
+     await this.getContainerList()
   }
   async stopContainer(containerId) {
 
@@ -56,6 +67,7 @@ export class DockerComponent implements OnInit {
       title: 'Stop Requested!',
       body: containerId
     })
+    await this.getContainerList()
   }
   async restartContainer(containerId) {
 
@@ -74,6 +86,61 @@ export class DockerComponent implements OnInit {
       title: 'Restart Requested!',
       body: containerId
     })
+    await this.getContainerList()
   }
+
+  async createChainDaemon() {
+      const containers = await fetch('http://localhost:36911/docker/containers/create', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({name: "lthn_chain", container:{
+          "Image": "lthn/chain:latest",
+            "HostConfig": {
+              NetworkMode: 'host'
+            },
+            "ExposedPorts": {
+            "48772/tcp": {},
+            "48782/tcp": {}
+            }
+        }})
+      })
+      //this.containers = await containers.json()
+      console.log(await containers.json())
+      this.notificationService.create({
+        type: NotificationType.html,
+        styleType: NotificationStyleType.success,
+        title: 'Container Create Requested!',
+        body: ''
+      })
+    await this.getContainerList()
+    }
+
+  public ngOnDestroy(): void {
+    this.sub.unsubscribe()
+  }
+
+
+    async removeContainer(containerId) {
+
+      const containers = await fetch('http://localhost:36911/docker/containers/delete', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({id: containerId})
+      })
+      //this.containers = await containers.json()
+      console.log( await containers.json())
+      this.notificationService.create({
+        type: NotificationType.html,
+        styleType: NotificationStyleType.success,
+        title: 'Restart Requested!',
+        body: containerId
+      })
+      await this.getContainerList()
+    }
+
 
 }
--- src/app/modules/docker/docker.module.ts
+++ src/app/modules/docker/docker.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DockerComponent } from './docker.component';
 import {NgxUIModule} from '@swimlane/ngx-ui';
+import {MatButtonModule} from '@angular/material/button';
 
 
 
@@ -11,7 +12,8 @@ import {NgxUIModule} from '@swimlane/ngx-ui';
   ],
 	imports: [
 		CommonModule,
-		NgxUIModule
+		NgxUIModule,
+		MatButtonModule
 	]
 })
 export class DockerModule { }
