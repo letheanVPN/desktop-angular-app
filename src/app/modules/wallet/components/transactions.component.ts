a0e90ac8ce6db49258d8eebdb7ee738c1a729651
Snider <snider@lt.hn>
2022-07-09 14:55:59 +0100

    updates tx UX

--- src/app/modules/wallet/components/transactions.component.ts
+++ src/app/modules/wallet/components/transactions.component.ts
@@ -29,7 +29,7 @@ export class TransactionsComponent implements OnInit {
 
 	columns = [
 		{name: 'Amount'}, {name: 'Fee'}, {name: 'Height'}, {name: 'Note'}, {name: 'Payment ID'},
-		{name: 'Timestamp'}, {name: 'txid'}, {name: 'Type'}, {name: 'Unlock Time', prop: "unlock_time"}];
+		{name: 'Timestamp'}, {name: 'txid'}, {name: 'Type', prop: 'type'}, {name: 'Unlock Time', prop: "unlock_time"}];
 
 	ColumnMode = ColumnMode;
 
